b11f3d0dc133841bf7e45c814c260b085fc9ffc7
marce <marcel.mtk42@gmail.com>
2022-03-22 17:38:26 +0100

    Zapomniałem na początu wrzucić projekt na githuba. -Do zadania dodałem DisplayMessageActivity.kt , w którym wyświetlą się dwa wyniki w textView & textView2 z editText i editText2 na MainActivity.

---
+++ app/src/main/java/com/example/relativelayoutapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.relativelayoutapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+
+        val editText2 = findViewById<EditText>(R.id.editText2)
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
